5632d5da0c51b69c03c6651c24f6e9dc6f9ad824
mvbattan <mvbattan@gmail.com>
2017-11-21 00:27:17 -0300

    Introducing onFailure effect

---
+++ src/effects/onFailure/test.js
@@ -0,0 +1,45 @@
+import Immutable from 'seamless-immutable';
+
+import createAction from '../../creators/createAction';
+import createReducer from '../../creators/createReducer';
+
+import onFailure from '.';
+
+const initialState = {
+  target: 'Some content',
+  targetLoading: true,
+  targetError: null
+};
+
+const setUp = {
+  state: null
+};
+
+beforeEach(() => {
+  setUp.state = Immutable(initialState);
+});
+
+describe('onFailure', () => {
+  it('Sets correctly error and loading', () => {
+    const reducer = createReducer(setUp.state, {
+      '@@ACTION/TYPE': onFailure()
+    });
+    const newState = reducer(setUp.state, createAction('@@ACTION/TYPE', 'target', 'Oops !'));
+    expect(newState).toEqual({
+      target: 'Some content',
+      targetLoading: false,
+      targetError: 'Oops !'
+    });
+  });
+  it('Sets conditionally error content', () => {
+    const reducer = createReducer(setUp.state, {
+      '@@ACTION/TYPE': onFailure(action => `Error: ${action.payload}`)
+    });
+    const newState = reducer(setUp.state, createAction('@@ACTION/TYPE', 'target', 'Oops !'));
+    expect(newState).toEqual({
+      target: 'Some content',
+      targetLoading: false,
+      targetError: 'Error: Oops !'
+    });
+  });
+});
