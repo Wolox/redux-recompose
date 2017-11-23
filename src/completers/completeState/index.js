f049453dbf09ef7938e34701f9d69f1bad2d46c8
mvbattan <mvbattan@gmail.com>
2017-11-23 00:32:40 -0300

    Providing negative tests for completeState

--- src/completers/completeState/index.js
+++ src/completers/completeState/index.js
@@ -1,5 +1,16 @@
 // Given a defaultState, it populates that state with ${key}Loading and ${key}Error
 function completeState(defaultState, ignoredTargets = []) {
+  if (!defaultState || defaultState.constructor !== Object) {
+    throw new Error('Expected an object as a state to complete.');
+  }
+  if (
+    !ignoredTargets ||
+    ignoredTargets.constructor !== Array ||
+    ignoredTargets.some(actionName => !actionName || typeof actionName !== 'string')
+  ) {
+    throw new Error('Expected an array of strings as ignored targets');
+  }
+
   const completedState = { ...defaultState };
   Object.keys(defaultState)
     .filter(key => ignoredTargets.indexOf(key) === -1)
--- src/completers/completeState/test.js
+++ src/completers/completeState/test.js
@@ -34,4 +34,13 @@ describe('completeState', () => {
       otherTarget: 2
     });
   });
+  it('Throws if an initial state is not provided', () => {
+    expect(() => completeState(null)).toThrow(Error, 'Expected an object as a state to complete.');
+  });
+  it('Throws if ignored targets is not a list', () => {
+    expect(() => completeState({}, {})).toThrow(Error, 'Expected an array of strings as ignored targets');
+  });
+  it('Throws if ignored targets is not a pure string array', () => {
+    expect(() => completeState({}, ['1', {}])).toThrow(Error, 'Expected an array of strings as ignored targets');
+  });
 });
