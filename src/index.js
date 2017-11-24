23ab9ed9d338c0a0b6a7c920bbf2c3f3cb50aa45
mvbattan <mvbattan@gmail.com>
2017-11-24 02:03:07 -0300

    Adding compilers for package

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import completeReducer from './completers/completeReducer';
+import completeState from './completers/completeState';
+
+import createAction from './creators/createAction';
+import createReducer from './creators/createReducer';
+import createTypes from './creators/createTypes';
+
+import onSuccess from './effects/onSuccess';
+import onFailure from './effects/onFailure';
+import onLoading from './effects/onLoading';
+import onLoaded from './effects/onLoaded';
+
+exports.completeReducer = completeReducer;
+exports.completeState = completeState;
+exports.createAction = createAction;
+exports.createReducer = createReducer;
+exports.createTypes = createTypes;
+exports.onSuccess = onSuccess;
+exports.onFailure = onFailure;
+exports.onLoading = onLoading;
+exports.onLoaded = onLoaded;
