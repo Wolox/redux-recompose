089fb47e2b2e8f481cdba30e57e161891389850c
mvbattan <manuel.battan@wolox.com.ar>
2018-01-16 14:29:38 -0300

    Introducing withFailure injection

---
+++ src/injections/withFailure/test.js
@@ -0,0 +1,67 @@
+import mockStore from '../../utils/asyncActionsUtils';
+import createTypes from '../../creators/createTypes';
+import composeInjections from '../composeInjections';
+import baseThunkAction from '../baseThunkAction';
+import withStatusHandling from '../withStatusHandling';
+
+import withFailure from '.';
+
+const MockService = {
+  fetchSomething: async () => new Promise(resolve => resolve({ ok: true, data: 42 })),
+  fetchFailureNotFound: async () => new Promise(resolve => resolve({ ok: false, problem: 'CLIENT_ERROR', status: 404 }))
+};
+
+const actions = createTypes(['FETCH', 'FETCH_SUCCESS', 'FETCH_FAILURE', 'OTHER_ACTION', 'NOT_FOUND'], '@TEST');
+
+describe('withFailure', () => {
+  it('Handles correctly failure behavior', async () => {
+    const store = mockStore({});
+    await store.dispatch(composeInjections(
+      baseThunkAction(actions.FETCH, 'aTarget', MockService.fetchSomething),
+      withFailure(dispatch => dispatch({ type: actions.OTHER_ACTION }))
+    ));
+    await store.dispatch(composeInjections(
+      baseThunkAction(actions.FETCH, 'aTarget', MockService.fetchFailureNotFound),
+      withFailure(dispatch => dispatch({ type: actions.OTHER_ACTION }))
+    ));
+    const actionsDispatched = store.getActions();
+    // Does not dispatch FAILURE action
+    expect(actionsDispatched).toEqual([
+      { type: actions.FETCH, target: 'aTarget' },
+      { type: actions.FETCH_SUCCESS, target: 'aTarget', payload: 42 },
+      { type: actions.FETCH, target: 'aTarget' },
+      { type: actions.OTHER_ACTION }
+    ]);
+  });
+  it('Does not execute depending on the status handler result', async () => {
+    const store = mockStore({});
+    // Does execute Failure handler
+    await store.dispatch(composeInjections(
+      baseThunkAction(actions.FETCH, 'aTarget', MockService.fetchFailureNotFound),
+      withStatusHandling({ 404: dispatch => dispatch({ type: actions.NOT_FOUND }) }),
+      withFailure(dispatch => dispatch({ type: actions.OTHER_ACTION }))
+    ));
+
+    // Does not execute Failure handler
+    await store.dispatch(composeInjections(
+      baseThunkAction(actions.FETCH, 'aTarget', MockService.fetchFailureNotFound),
+      withStatusHandling({
+        404: dispatch => {
+          dispatch({ type: actions.NOT_FOUND });
+          return false;
+        }
+      }),
+      withFailure(dispatch => dispatch({ type: actions.OTHER_ACTION }))
+    ));
+
+    const actionsDispatched = store.getActions();
+    // Does not dispatch FAILURE action
+    expect(actionsDispatched).toEqual([
+      { type: actions.FETCH, target: 'aTarget' },
+      { type: actions.NOT_FOUND },
+      { type: actions.OTHER_ACTION },
+      { type: actions.FETCH, target: 'aTarget' },
+      { type: actions.NOT_FOUND }
+    ]);
+  });
+});
