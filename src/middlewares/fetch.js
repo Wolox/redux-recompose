e2d53b9b73868ff060a9ffc73a0e3fcf960497b9
Manuel Battan <manuel.battan@wolox.com.ar>
2018-03-06 16:26:35 -0300

    Adding singleCallThunkAction without tests

--- src/middlewares/fetch.js
+++ src/middlewares/fetch.js
@@ -1,10 +1,16 @@
 import baseThunkAction from '../injections/baseThunkAction';
 import emptyThunkAction from '../injections/emptyThunkAction';
+import singleCallThunkAction from '../injections/singleCallThunkAction';
 import composeInjections from '../injections/composeInjections';
 import mergeInjections from '../injections/mergeInjections';
 
 const ensembleInjections = action => {
-  const base = action.target ? baseThunkAction(action) : emptyThunkAction(action);
+  let base;
+  if (!action.type) {
+    base = singleCallThunkAction(action);
+  } else {
+    base = action.target ? baseThunkAction(action) : emptyThunkAction(action);
+  }
   if (!action.injections) return base;
 
   const injections = action.injections.constructor === Array
