1b83891d702bd97b4e7280627ac0381169e5c8b1
Manuel Battan <manuel.battan@wolox.com.ar>
2018-06-22 11:49:20 -0300

    Customizing invisible reducer

--- src/invisible/wrapCombineReducers/test.js
+++ src/invisible/wrapCombineReducers/test.js
@@ -1,14 +1,37 @@
 import { combineReducers, createStore } from 'redux';
 
+import createReducer from '../../creators/createReducer';
+
 import wrapCombineReducers, { createExternalActions } from '.';
+
+const setUp = {
+  store: null
+};
+
+const configureStore = invisibleReducer => {
+  const reducersObject = { foo: (state = { NSLoading: false }) => state };
+  const ownCombineReducers = wrapCombineReducers(combineReducers, invisibleReducer);
+  return createStore(ownCombineReducers(reducersObject));
+};
+
+beforeEach(() => {
+  setUp.store = configureStore();
+});
 
 describe('wrapCombineReducers', () => {
   it('Wrap reducers can combine reducers and provide an invisible reducer', async () => {
-    const reducersObject = { foo: (state = { NSLoading: false }) => state };
-    const ownCombineReducers = wrapCombineReducers(combineReducers);
-    const store = createStore(ownCombineReducers(reducersObject));
     const $ = createExternalActions('foo');
-    await store.dispatch({ type: $.LOADING, target: 'NS' });
-    expect(store.getState()).toEqual({ foo: { NSLoading: true } });
+    await setUp.store.dispatch({ type: $.LOADING, target: 'NS' });
+    expect(setUp.store.getState()).toEqual({ foo: { NSLoading: true } });
+  });
+  it('Allow to customize the invisible reducer', () => {
+    const invisibleReducer = createReducer(
+      {},
+      { INCREMENT: state => ({ ...state, counter: (state.counter || 0) + 1 }) }
+    );
+    const store = configureStore(invisibleReducer);
+    const $ = createExternalActions('foo', ['INCREMENT']);
+    store.dispatch({ type: $.INCREMENT });
+    expect(store.getState()).toEqual({ foo: { NSLoading: false, counter: 1 } });
   });
 });
