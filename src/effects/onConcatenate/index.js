1eb237e50e6d48f30c440518270e5c93d1c0900b
Mariano Zicavo <marianozicavo@users.noreply.github.com>
2018-11-14 11:10:23 -0300

    Use concat instead of spread operator

--- src/effects/onConcatenate/index.js
+++ src/effects/onConcatenate/index.js
@@ -4,7 +4,7 @@ function onConcatenate(selector = action => action.payload) {
   return validate({
     name: 'onConcatenate',
     realTarget: action => action.target,
-    do: (action, state) => [...state[action.target], ...selector(action)]
+    do: (action, state) => state[action.target].concat(selector(action))
   });
 }
 
