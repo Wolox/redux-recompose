50279325c31b85e837d114dbb22a4b7ac61ed37c
Santiago <santinucera4@gmail.com>
2019-02-25 16:14:16 -0300

    Format

--- src/utils/typeUtils.js
+++ src/utils/typeUtils.js
@@ -1,11 +1,10 @@
 export const isArray = array => Object.prototype.toString.call(array) === '[object Array]';
 
-export const isStringArray = array => !(
-  !array ||
-  !isArray(array) ||
-  array.some(actionName => !actionName || typeof actionName !== 'string')
-);
+export const isStringArray = array =>
+  !(
+    !array ||
+    !isArray(array) ||
+    array.some(actionName => !actionName || typeof actionName !== 'string')
+  );
 
-export const isValidObject = obj => !(
-  !obj || obj.constructor !== Object
-);
+export const isValidObject = obj => !(!obj || obj.constructor !== Object);
