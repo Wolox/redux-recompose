e1023f2cb362af79e7b3c54c6dc01df635f0bbf4
Jeymarson Joel Garcia Sanmartin <jeymarson.garcia@wolox.co>
2019-07-10 10:35:49 -0500

    on replace effect

---
+++ src/effects/onReplace/index.js
@@ -0,0 +1,23 @@
+import validate from '../validate';
+
+function onReplace() {
+  return validate({
+    name: 'onReplace',
+    realTarget: action => action.target,
+    do: (action, state) => {
+      const { payload, index, condition, target } = action;
+      let array = [...state[target]];
+      if (index) array[index] = payload;
+      else if (condition) {
+        const arrayFilter = array.filter(condition);
+        arrayFilter.forEach(item => {
+          const i = array.indexOf(item);
+          array[i] = payload;
+        })
+      }
+      return [...array];
+    }
+  });
+}
+
+export default onReplace;
