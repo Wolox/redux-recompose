8623286fb1a56b8dc004fa8345dd7f59335b276f
Jeymarson Joel Garcia Sanmartin <jeymarson.garcia@wolox.co>
2019-08-13 15:46:49 -0500

    fix onreplace effect

--- src/effects/onReplace/index.js
+++ src/effects/onReplace/index.js
@@ -9,14 +9,11 @@ function onReplace() {
         payload, index, condition, target
       } = action;
       const array = [...state[target]];
-      if (index) array[index] = payload;
-      else if (condition) {
+      if (condition) {
         const arrayFilter = array.filter(condition);
-        arrayFilter.forEach(item => {
-          const i = array.indexOf(item);
-          array[i] = payload;
-        });
+        return array.map(item => (arrayFilter.includes(item) ? payload : item));
       }
+      if (index) array[index] = payload;
       return [...array];
     }
   });
