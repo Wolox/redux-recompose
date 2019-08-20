0102f66784e4a69502fbff3c922e75a043f7c555
Mariano Zicavo <marianozicavo@gmail.com>
2019-08-20 18:11:03 -0400

    Throw error if injections is not an array

--- src/injections/mergeInjections/index.js
+++ src/injections/mergeInjections/index.js
@@ -1,4 +1,7 @@
 function mergeInjections(injections) {
+  if (injections.constructor !== Array) {
+    throw new TypeError('Expected actions injections to be an array');
+  }
   return injections.reduce((a, b) => ({ ...a, ...b }), {});
 }
 
