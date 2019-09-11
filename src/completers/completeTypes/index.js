e2609c0ba7643429e5f7ad104d5ce5a3492a2689
Santiago Nucera <santinucera4@gmail.com>
2019-09-11 18:23:04 -0300

    Remove unncesary code form compleType

--- src/completers/completeTypes/index.js
+++ src/completers/completeTypes/index.js
@@ -1,13 +1,12 @@
 import { isStringArray } from '../../utils/typeUtils';
 
-export const primaryActionsCompleter = primaryActions => primaryActions.reduce(
+const primaryActionsCompleter = primaryActions => primaryActions.reduce(
   (completedTypes, type) => [...completedTypes, type, `${type}_SUCCESS`, `${type}_FAILURE`],
   []
 );
 
-export const ignoredActionsCompleter = ignoredActions => ignoredActions || [];
 
-function completeTypes({ primaryActions, ignoredActions = [], customCompleters = [] }) {
+function completeTypes({ primaryActions = [], ignoredActions = [], customCompleters = [] }) {
   if (!isStringArray(primaryActions)) {
     throw new Error('Primary actions must be an array of strings');
   }
@@ -18,15 +17,13 @@ function completeTypes({ primaryActions, ignoredActions = [], customCompleters =
     if (!isStringArray(actions)) throw new Error('Exception cases from actions must be an array of strings');
     if (typeof completer !== 'function') throw new Error('Completer must be a function');
   });
-
   const primaryTypes = primaryActionsCompleter(primaryActions);
-  const ignoredTypes = ignoredActionsCompleter(ignoredActions);
   const customCompletedTypes = customCompleters.reduce(
     (customTypes, { actions, completer }) => [...customTypes, ...actions.flatMap(action => completer(action))],
     []
   );
 
-  return [...primaryTypes, ...ignoredTypes, ...customCompletedTypes];
+  return [...primaryTypes, ...ignoredActions, ...customCompletedTypes];
 }
 
 export default completeTypes;
