edbd5b8485a2f9d7d56d156a28bd99ccca831b7d
Santiago Nucera <santinucera4@gmail.com>
2019-09-17 15:15:16 -0300

    Refactor how to calculate customCompleteTypes

--- src/completers/completeTypes/index.js
+++ src/completers/completeTypes/index.js
@@ -18,14 +18,9 @@ function completeTypes({ primaryActions, ignoredActions = [], customCompleters =
     if (!isStringArray(actions)) throw new Error('Exception cases from actions must be an array of strings');
     if (typeof completer !== 'function') throw new Error('Completer must be a function');
   });
-
   const primaryTypes = primaryActionsCompleter(primaryActions);
   const ignoredTypes = ignoredActionsCompleter(ignoredActions);
-  const customCompletedTypes = customCompleters.reduce(
-    (customTypes, { actions, completer }) => [...customTypes, ...actions.flatMap(action => completer(action))],
-    []
-  );
-
+  const customCompletedTypes = customCompleters.flatMap(({ actions, completer }) => actions.flatMap(action => completer(action)));
   return [...primaryTypes, ...ignoredTypes, ...customCompletedTypes];
 }
 
