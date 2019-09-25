12db2a99513bdeaacbd50a838f6a95036c3b8ac8
Anisospina <anisospina@Woloxs-MacBook-Pro-Ana.local>
2019-09-24 20:13:10 -0500

    test isObjectArray

---
+++ src/utils/test.js
@@ -0,0 +1,20 @@
+import { isObjectArray } from './typeUtils';
+
+describe('Is object array', () => {
+  it('Shouldn`t receive number as param', () => {
+    const result = isObjectArray(100);
+    expect(result).toBe(false);
+  });
+
+  it('Should receive array', () => {
+    const result = isObjectArray([]);
+    expect(result).toBe(true);
+  });
+
+  it('should receibe only objects array', () => {
+    const result = isObjectArray([{}]);
+    expect(result).toBe(true);
+    const result2 = isObjectArray([{}, {}, null]);
+    expect(result2).toBe(false);
+  });
+});
