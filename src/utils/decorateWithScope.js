997a7e8006c49aa8d9ebe39a7c020d5da65bb04b
Dorian Alvarado <dorianalvarado@MacBook-Pro-de-wolox.local>
2019-12-13 12:32:07 -0300

    Made default export and disable param reassign

--- src/utils/decorateWithScope.js
+++ src/utils/decorateWithScope.js
@@ -1,18 +1,22 @@
-export const decorateWithScope = (scopes, actionCreators) => {
-    const asyncRegex = /async/;
-    return Object.entries(actionCreators).reduce((result, [actionName, actionCreator]) => {
-      if (asyncRegex.test(actionCreator.toString())) {
-        result[actionName] = (...args) => {
-          const asyncAction = actionCreator(...args);
-          asyncAction.scopes = scopes[actionName];
-          return asyncAction;
-        };
-        return result;
-      }
-      result[actionName] = (...args) => ({
-        ...actionCreator(...args),
-        scopes: scopes[actionName]
-      });
+const decorateWithScope = (scopes, actionCreators) => {
+  const asyncRegex = /async/;
+  return Object.entries(actionCreators).reduce((result, [actionName, actionCreator]) => {
+    if (asyncRegex.test(actionCreator.toString())) {
+      // eslint-disable-next-line no-param-reassign
+      result[actionName] = (...args) => {
+        const asyncAction = actionCreator(...args);
+        asyncAction.scopes = scopes[actionName];
+        return asyncAction;
+      };
       return result;
-    }, {});
-  };
+    }
+    // eslint-disable-next-line no-param-reassign
+    result[actionName] = (...args) => ({
+      ...actionCreator(...args),
+      scopes: scopes[actionName]
+    });
+    return result;
+  }, {});
+};
+
+export default decorateWithScope;
