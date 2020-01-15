44c2342b85f1d9c34b96292d23382b75e793d872
Felipe Rodriguez Esturo <felipe.rodriguez@wolox.com.ar>
2020-01-15 09:30:23 -0300

    handle pagination

--- src/injections/baseThunkAction/index.js
+++ src/injections/baseThunkAction/index.js
@@ -3,7 +3,7 @@ function baseThunkAction({
   target,
   service,
   payload = () => {},
-  paginationAction,
+  paginationAction = false,
   reducerName,
   refresh = true,
   successSelector = response => response.data,
@@ -11,24 +11,23 @@ function baseThunkAction({
 }) {
   const pageSelector = state =>
     paginationAction && {
-      totalPages: state[reducerName][`${target}TotalPages`],
       nextPage: refresh ? 1 : state[reducerName][`${target}NextPage`]
     };
-  const selector = typeof payload === "function" ? payload : () => payload;
+  const selector = typeof payload === 'function' ? payload : () => payload;
 
   const finalSelector = state =>
-    paginationAction
-      ? { ...pageSelector(state), ...selector(state) }
-      : selector(state);
+    (paginationAction ? { ...pageSelector(state), ...selector(state) } : selector(state));
   return {
     prebehavior: dispatch => dispatch({ type, target }),
     apiCall: async getState => service(finalSelector(getState())),
     determination: response => response.ok,
+    paginationAction,
+    pageSelector: { reducerName, target },
     success: (dispatch, response) =>
       dispatch({
         type: `${type}_SUCCESS`,
         target,
-        payload: successSelector(response)
+        payload: response && successSelector(response)
       }),
     failure: (dispatch, response) =>
       dispatch({
