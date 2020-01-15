1ac405e6e74cbbc9079cbb01f68ae6db86acbcee
Felipe Rodriguez Esturo <felipe.rodriguez@wolox.com.ar>
2020-01-15 08:58:21 -0300

    started pagination

--- src/injections/baseThunkAction/index.js
+++ src/injections/baseThunkAction/index.js
@@ -3,17 +3,39 @@ function baseThunkAction({
   target,
   service,
   payload = () => {},
+  paginationAction,
+  reducerName,
+  refresh = true,
   successSelector = response => response.data,
   failureSelector = response => response.problem
 }) {
-  const selector = typeof payload === 'function' ? payload : () => payload;
+  const pageSelector = state =>
+    paginationAction && {
+      totalPages: state[reducerName][`${target}TotalPages`],
+      nextPage: refresh ? 1 : state[reducerName][`${target}NextPage`]
+    };
+  const selector = typeof payload === "function" ? payload : () => payload;
 
+  const finalSelector = state =>
+    paginationAction
+      ? { ...pageSelector(state), ...selector(state) }
+      : selector(state);
   return {
     prebehavior: dispatch => dispatch({ type, target }),
-    apiCall: async getState => service(selector(getState())),
+    apiCall: async getState => service(finalSelector(getState())),
     determination: response => response.ok,
-    success: (dispatch, response) => dispatch({ type: `${type}_SUCCESS`, target, payload: successSelector(response) }),
-    failure: (dispatch, response) => dispatch({ type: `${type}_FAILURE`, target, payload: failureSelector(response) })
+    success: (dispatch, response) =>
+      dispatch({
+        type: `${type}_SUCCESS`,
+        target,
+        payload: successSelector(response)
+      }),
+    failure: (dispatch, response) =>
+      dispatch({
+        type: `${type}_FAILURE`,
+        target,
+        payload: failureSelector(response)
+      })
   };
 }
 
